0b46ae18bb4c12a164ce0f4903e7da7ee62867d8
Reza Samani <70468060+codaux@users.noreply.github.com>
2026-09-20 17:57:26 -0400

    Run candidate finder in a Web Worker

---
+++ workers/finder.worker.ts
@@ -0,0 +1,26 @@
+/// <reference lib="webworker" />
+
+import { enumeratePolyominoes } from "../lib/generator";
+import { rankFinderResults, screenShape } from "../lib/finder";
+
+type Request = {
+  cellCount:number;
+  limit:number;
+  allowReflection:boolean;
+  maxTiles:number;
+};
+
+self.onmessage=(event:MessageEvent<Request>)=>{
+  const {cellCount,limit,allowReflection,maxTiles}=event.data;
+  const shapes=enumeratePolyominoes(cellCount,limit);
+  const results=[];
+  for(let i=0;i<shapes.length;i++){
+    results.push(screenShape(shapes[i],allowReflection,maxTiles));
+    if(i%5===0 || i===shapes.length-1){
+      self.postMessage({type:"progress",done:i+1,total:shapes.length});
+    }
+  }
+  self.postMessage({type:"done",results:rankFinderResults(results)});
+};
+
+export {};
